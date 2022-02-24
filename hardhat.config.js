6c9a95d54bfbb3517d8b39a3718e0db89e8cce85
Abdullah Furkan Ozbek <abdullah.furkan.ozbek@gmail.com>
2022-02-24 10:45:03 +0300

    Smart contract deployed to rinkeby network with hardhat

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,16 +1,25 @@
+require("dotenv").config();
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
+
+require("./tasks/faucet");
+
+const { ALCHEMY_API_URL, PK } = process.env;
 
 // The next line is part of the sample project, you don't need it in your
 // project. It imports a Hardhat task definition, that can be used for
 // testing the frontend.
-require("./tasks/faucet");
 
 // If you are using MetaMask, be sure to change the chainId to 1337
 module.exports = {
   solidity: "0.7.3",
   networks: {
     hardhat: {
-      chainId: 31337
-    }
-  }
+      chainId: 31337,
+    },
+    rinkeby: {
+      url: ALCHEMY_API_URL,
+      accounts: [`0x${PK}`],
+    },
+  },
 };
